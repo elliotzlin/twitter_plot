7b407449b8bee9b288e35e5fa1bf65c516101925
Elliot Lin <elliot.z.lin@berkeley.edu>
2017-02-12 04:36:29 -0800

    Add json files and implement map coloring + top trending

--- src/js/viewModels/dashboard.js
+++ src/js/viewModels/dashboard.js
@@ -7,101 +7,40 @@
  */
 define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojchart', 'ojs/ojthematicmap'],
  function(oj, ko, $) {
-
     function DashboardViewModel() {
       var self = this;
-      // Below are a subset of the ViewModel methods invoked by the ojModule binding
-      // Please reference the ojModule jsDoc for additionaly available methods.
-      var colorHandler = new oj.ColorAttributeGroupHandler();
-              this.lineWithAreaColor = colorHandler.getValue('lineWithAreaColor');
+      var countries = {"Canada": "CAN", "Egypt": "EGY", "France": "FRA", "Japan": "JPN", "United States": "USA"};
+      var trend_data = [];
+      $.getJSON("data/World.json",
+          function(data) {
+              console.log(data);
+              trend_data = data;
+              self.layers = [{layer: 'countries', areaDataLayer: {id: 'adl1', areas: getTrendsData}}];
+          }
+      );
 
-              /* chart data */
-              var values = [5, 8, 2, 7, 0, 9, 2, 3, 4, 2];
-
-              this.sparkValues = ko.observableArray(values);
-
-              var rangeValues = [{low: 5, high: 9},
-                                 {low: 3, high: 7},
-                                 {low: 3, high: 8},
-                                 {low: 5, high: 10},
-                                 {low: 7, high: 14},
-                                 {low: 8, high: 13},
-                                 {low: 6, high: 11},
-                                 {low: 4, high: 9},
-                                 {low: 1, high: 5},
-                                 {low: 2, high: 8}];
-
-              this.sparkRangeValues = ko.observableArray(rangeValues);
-      /**
-       * Optional ViewModel method invoked when this ViewModel is about to be
-       * used for the View transition.  The application can put data fetch logic
-       * here that can return a Promise which will delay the handleAttached function
-       * call below until the Promise is resolved.
-       * @param {Object} info - An object with the following key-value pairs:
-       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-       * @param {Function} info.valueAccessor - The binding's value accessor.
-       * @return {Promise|undefined} - If the callback returns a Promise, the next phase (attaching DOM) will be delayed until
-       * the promise is resolved
-       */
-      self.handleActivated = function(info) {
-        // Implement if needed
+      var getCountryID = function(country) {
+          return countries[country];
       };
 
-      /**
-       * Optional ViewModel method invoked after the View is inserted into the
-       * document DOM.  The application can put logic that requires the DOM being
-       * attached here.
-       * @param {Object} info - An object with the following key-value pairs:
-       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-       * @param {Function} info.valueAccessor - The binding's value accessor.
-       * @param {boolean} info.fromCache - A boolean indicating whether the module was retrieved from cache.
-       */
-      self.handleAttached = function(info) {
-        // Implement if needed
-      };
+      var getTrendsData = function() {
+          var areaData = [];
+          console.log(trend_data);
+          for (var i = 0; i < trend_data.length; i++) {
+              var top = trend_data[i]["trends"][0]["name"];
+              areaData.push(
+                  { id: i.toString(),
+                    color: '#42C0FB',
+                    location: getCountryID(trend_data[i]["locations"][0]["name"]),
+                    shortDesc: 'Top trending tweet - ' + top.toString()}
+              );
+          }
+          return areaData;
+      }
 
-
-      /**
-       * Optional ViewModel method invoked after the bindings are applied on this View.
-       * If the current View is retrieved from cache, the bindings will not be re-applied
-       * and this callback will not be invoked.
-       * @param {Object} info - An object with the following key-value pairs:
-       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-       * @param {Function} info.valueAccessor - The binding's value accessor.
-       */
-      self.handleBindingsApplied = function(info) {
-        // Implement if needed
-      };
-
-      /*
-       * Optional ViewModel method invoked after the View is removed from the
-       * document DOM.
-       * @param {Object} info - An object with the following key-value pairs:
-       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-       * @param {Function} info.valueAccessor - The binding's value accessor.
-       * @param {Array} info.cachedNodes - An Array containing cached nodes for the View if the cache is enabled.
-       */
-      self.handleDetached = function(info) {
-        // Implement if needed
-      };
-
-      self.layers = [{layer: 'countries'}];
+      self.layers = [{layer: 'countries', areaDataLayer: {id: 'adl1', areas: getTrendsData}}];
     }
 
-    var dashboard = new DashboardViewModel()
-
-    $(document).ready(
-    function()
-    {
-      ko.applyBindings(chartModel, document.getElementById('sparkChart-container'));
-    }
-    );
-
-    /*
-     * Returns a constructor for the ViewModel so that the ViewModel is constrcuted
-     * each time the view is displayed.  Return an instance of the ViewModel if
-     * only one instance of the ViewModel is needed.
-     */
     return new DashboardViewModel();
   }
 );
